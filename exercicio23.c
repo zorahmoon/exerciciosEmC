fd9f185d10089dfcb056a6029e619f52d59826d7
zorahmoon <ester.miranda.cobos@gmail.com>
2024-09-13 11:59:19 -0300

    vigésimo terceiro commit

---
+++ exercicio23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int A, B, Soma;
+
+printf("Digite o valor de A: ");
+scanf("%d", &A);
+
+printf("Digite o valor de B: ");
+scanf("%d", &B);
+
+Soma = A + B;
+
+printf("A soma de A e B é de: %.2f\n", Soma);
+
+
+}
