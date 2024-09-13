c365303353f6c6cbfc4417bd77a255b852534cf5
zorahmoon <ester.miranda.cobos@gmail.com>
2024-09-13 11:02:33 -0300

    décimo sétimo commit

---
+++ exercicio17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    float fahrenheit, celsius;
+
+printf("Digite a temperatura em Fahrenheit: ");
+scanf("%f", &fahrenheit);
+
+celsius = (fahrenheit - 32) * 5/9;
+
+printf("A temperatura em Celsius é: %.2f°c\n", celsius);
+
+return 0;
+
+}
